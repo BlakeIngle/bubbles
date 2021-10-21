89573d14a8e1df055979c85c2b36ecd85f33489b
BlakeIngle <blake.ingle7@gmail.com>
2021-10-21 15:01:14 -0400

    added more controls to circles animation to make it more fun

--- src/app/circle-warp/circle-warp.component.ts
+++ src/app/circle-warp/circle-warp.component.ts
@@ -11,7 +11,8 @@ export class CircleWarpComponent implements OnInit {
   layerCount: number = 100; // how many circles in a stack
   ringCount: number = 5; // how many circles in a circle
 
-  radius: number = 100; // radius of total circle
+  innerRadius: number = 100; // radius of total circle for small circles
+  outerRadius: number = 100; // radius of total circle for large circles
 
   minRadius: number = 10; // radius of smallest circle in a stack
   maxRadius: number = 100; // radius of largest circle in a stack
@@ -20,10 +21,8 @@ export class CircleWarpComponent implements OnInit {
   largeVelocity: number = 0.00001; // velocity of largest circle in a stack
   // large velocity < small velocity - smaller objects have less mass
 
-  layers: {
-    small: { r: number, theta: number },
-    large: { r: number, theta: number }
-  }
+  smallTheta: number = 0; // rotation of smallest layer
+  largeTheta: number = 0; // rotation of largest layer
 
   constructor() { }
 
@@ -40,10 +39,6 @@ export class CircleWarpComponent implements OnInit {
 
       p.setup = () => {
         p.createCanvas(window.innerWidth, window.innerHeight);
-        this.layers = {
-          small: { r: this.minRadius, theta: 0 },
-          large: { r: this.maxRadius, theta: 0 }
-        };
       }
 
       p.draw = () => {
@@ -60,8 +55,8 @@ export class CircleWarpComponent implements OnInit {
   }
 
   moveLayers(p) {
-    this.layers.small.theta = (this.layers.small.theta + this.smallVelocity) //% (Math.PI * 2);
-    this.layers.large.theta = (this.layers.large.theta + this.largeVelocity) //% (Math.PI * 2);
+    this.smallTheta = (this.smallTheta + this.smallVelocity) //% (Math.PI * 2);
+    this.largeTheta = (this.largeTheta + this.largeVelocity) //% (Math.PI * 2);
     // rotate % 2pi
     // modulating by 2PI causes snapping
   }
@@ -70,18 +65,20 @@ export class CircleWarpComponent implements OnInit {
     // draw all circles in a layer based on number of layers
     p.push();
     p.translate(window.innerWidth / 2, window.innerHeight / 2)
-    p.fill(255, 5);
+    let opacity = 255 / this.layerCount
+    p.fill(255, opacity);
     p.noStroke();
 
     for (let i = 0; i < this.layerCount; i++) {
       // draw one layer
-      let layerTheta = p.map(i, 0, this.layerCount - 1, this.layers.small.theta, this.layers.large.theta);
-      let layerR = p.map(i, 0, this.layerCount - 1, this.layers.small.r, this.layers.large.r);
+      let layerTheta = p.map(i, 0, this.layerCount - 1, this.smallTheta, this.largeTheta);
+      let layerCircleR = p.map(i, 0, this.layerCount - 1, this.minRadius, this.maxRadius);
+      let layerPolarR = p.map(i, 0, this.layerCount - 1, this.innerRadius, this.outerRadius);
       for (let j = 0; j < this.ringCount; j++) {
         let circleTheta = (Math.PI * 2 / this.ringCount) * j
-        let x = this.radius * p.sin(circleTheta + layerTheta);
-        let y = this.radius * p.cos(circleTheta + layerTheta);
-        p.circle(x, y, layerR)
+        let x = layerPolarR * p.sin(circleTheta + layerTheta);
+        let y = layerPolarR * p.cos(circleTheta + layerTheta);
+        p.circle(x, y, layerCircleR)
       }
     }
 
